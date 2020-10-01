271fa26dd18595962a762e648dd4381622951487
Marius van der Wijden <m.vanderwijden@live.de>
2020-10-01 12:56:10 +0200

    executor: added executor

--- executor/executor.go
+++ executor/executor.go
@@ -16,3 +16,80 @@
 
 // Package executor executes state tests and compares results.
 package executor
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/holiman/goevmlab/evms"
+)
+
+var (
+	vms = []evms.Evm{
+		evms.NewGethEVM("/home/matematik/go/src/github.com/ethereum/go-ethereum/build/bin/evm"),
+		evms.NewParityVM("/home/matematik/ethereum/openethereum/target/release/openethereum-evm"),
+		evms.NewNethermindVM("/home/matematik/ethereum/nethermind/nethtest"),
+		//evms.NewBesuVM("/home/matematik/ethereum/besu/ethereum/evmtool/build/install/evmtool/bin/evm"),
+	}
+)
+
+func Execute(dirName string) error {
+	infos, err := ioutil.ReadDir(dirName)
+	if err != nil {
+		return err
+	}
+	for _, info := range infos {
+		// All generated tests end in .json
+		if strings.HasSuffix(info.Name(), ".json") {
+			var (
+				testFile  = info.Name()
+				testName  = strings.TrimRight(testFile, ".json")
+				traceName = fmt.Sprintf("%v-trace.jsonl")
+			)
+			outputs := executeTest(testFile)
+			if !verify(traceName, outputs) {
+				if err := dump(testName, vms, outputs); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	// All tests sucessfully executed
+	return nil
+}
+
+// executeTest executes a state test
+func executeTest(testName string) []bytes.Buffer {
+	buf := make([]bytes.Buffer, len(vms))
+	for i, vm := range vms {
+		vm.RunStateTest(testName, &buf[i], false)
+	}
+	return buf
+}
+
+// verify checks if the traces match the default trace.
+func verify(traceName string, outputs []bytes.Buffer) bool {
+	ref, err := ioutil.ReadFile(traceName)
+	if err != nil {
+		panic(err)
+	}
+	for _, out := range outputs {
+		if !bytes.Equal(ref, out.Bytes()) {
+			return false
+		}
+	}
+	return true
+}
+
+// dump writes outputs to a file in case of a verification problem
+func dump(filename string, vms []evms.Evm, outputs []bytes.Buffer) error {
+	for i, out := range outputs {
+		filename := fmt.Sprintf("%v-%v-trace.jsonl", filename, vms[i].Name())
+		if err := ioutil.WriteFile(filename, out.Bytes(), os.ModeAppend); err != nil {
+			return err
+		}
+	}
+}
