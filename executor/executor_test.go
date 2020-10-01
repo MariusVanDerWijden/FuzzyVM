8aaf40d5c1b6933cb5fbb94856a73edb30ce2584
Marius van der Wijden <m.vanderwijden@live.de>
2020-10-01 17:24:28 +0200

    executor: added tests for executor

---
+++ executor/executor_test.go
@@ -0,0 +1,26 @@
+// Copyright 2020 Marius van der Wijden
+// This file is part of the fuzzy-vm library.
+//
+// The fuzzy-vm library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The fuzzy-vm library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the fuzzy-vm library. If not, see <http://www.gnu.org/licenses/>.
+
+package executor
+
+import "testing"
+
+func TestExecute(t *testing.T) {
+	err := Execute("../out", "../crashes")
+	if err != nil {
+		t.Fail()
+	}
+}
