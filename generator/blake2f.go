29429ec83449eb370a2c02e1944b6dde708366ce
Marius van der Wijden <m.vanderwijden@live.de>
2020-10-04 19:06:35 +0200

    generator: added blake2f generator

---
+++ generator/blake2f.go
@@ -0,0 +1,77 @@
+// Copyright 2020 Marius van der Wijden
+// This file is part of the fuzzy-vm library.
+//
+// The fuzzy-vm library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The fuzzy-vm library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the fuzzy-vm library. If not, see <http://www.gnu.org/licenses/>.
+
+package generator
+
+import (
+	"encoding/binary"
+
+	"github.com/MariusVanDerWijden/FuzzyVM/filler"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/holiman/goevmlab/program"
+)
+
+var blake2fAddr = common.HexToAddress("0x9")
+
+type blake2fCaller struct{}
+
+func (*blake2fCaller) call(p *program.Program, f *filler.Filler) error {
+	var (
+		input  = make([]byte, 213)
+		offset = 0
+	)
+	// Rounds
+	binary.BigEndian.PutUint32(input, f.Uint32())
+	offset += 4
+	// h
+	for i := 0; i < 8; i++ {
+		binary.BigEndian.PutUint64(input[offset:], f.Uint64())
+		offset += 8
+	}
+	// m
+	for i := 0; i < 16; i++ {
+		binary.BigEndian.PutUint64(input[offset:], f.Uint64())
+		offset += 8
+	}
+	// t
+	for i := 0; i < 2; i++ {
+		binary.BigEndian.PutUint64(input[offset:], f.Uint64())
+		offset += 8
+	}
+	// Valid or invalid inputs
+	if f.Bool() {
+		input[212] = f.Byte()
+	} else {
+		if f.Bool() {
+			input[212] = 0
+		} else {
+			input[212] = 1
+		}
+	}
+
+	c := callObj{
+		gas:       f.BigInt(),
+		address:   blake2fAddr,
+		inOffset:  0,
+		inSize:    213,
+		outOffset: 0,
+		outSize:   64,
+		value:     f.BigInt(),
+	}
+	p.Mstore(input, 0)
+	callRandomizer(p, f, c)
+	return nil
+}
