e6bc1c4d2275315ef86329076a46ae9a7ee2ca33
Marius van der Wijden <m.vanderwijden@live.de>
2021-04-27 18:51:52 +0200

    generator: split strategies

--- generator/basic_strategies.go
+++ generator/basic_strategies.go
@@ -17,27 +17,16 @@
 package generator
 
 import (
-	"math/big"
-
-	"github.com/MariusVanDerWijden/FuzzyVM/filler"
-	"github.com/MariusVanDerWijden/FuzzyVM/generator/precompiles"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/holiman/goevmlab/ops"
 )
 
 var basicStrategies = []Strategy{
 	new(opcodeGenerator),
-	new(jumpdestGenerator),
-	new(jumpGenerator),
 	new(memStorageGenerator),
 	new(mstoreGenerator),
 	new(sstoreGenerator),
 	new(returnDataGenerator),
 	new(returnGenerator),
-	new(createCallRNGGenerator),
-	new(createCallGenerator),
-	new(randomCallGenerator),
-	new(callPrecompileGenerator),
 	new(pushGenerator),
 }
 
@@ -56,52 +45,6 @@ func (*opcodeGenerator) Execute(env Environment) {
 
 func (*opcodeGenerator) Importance() int {
 	return 10
-}
-
-type jumpdestGenerator struct{}
-
-func (*jumpdestGenerator) Execute(env Environment) {
-	switch env.f.Byte() % 10 {
-	case 0:
-		// Set a jumpdest label
-		env.jumptable.Push(env.p.Label(), env.p.Label())
-	case 1:
-		// Set the jumpdest randomly
-		env.jumptable.Push(uint64(env.f.Uint16()), env.p.Label())
-	default:
-		// Set a jumpdest
-		env.jumptable.Push(env.p.Jumpdest(), env.p.Label())
-	}
-}
-
-func (*jumpdestGenerator) Importance() int {
-	return 5
-}
-
-type jumpGenerator struct{}
-
-func (*jumpGenerator) Execute(env Environment) {
-	if env.f.Bool() {
-		// Jump to a label
-		jumpdest := env.jumptable.Pop(env.p.Label())
-		env.p.Jump(jumpdest)
-	} else {
-		// Jumpi to a label
-		var (
-			jumpdest   = env.jumptable.Pop(env.p.Label())
-			shouldJump = env.f.Bool()
-			condition  = big.NewInt(0)
-		)
-		if shouldJump {
-			condition = env.f.BigInt()
-		}
-		// jumps if condition != 0
-		env.p.JumpIf(jumpdest, condition)
-	}
-}
-
-func (*jumpGenerator) Importance() int {
-	return 7
 }
 
 type memStorageGenerator struct{}
@@ -175,88 +118,6 @@ func (*returnGenerator) Execute(env Environment) {
 
 func (*returnGenerator) Importance() int {
 	return 1
-}
-
-type createCallRNGGenerator struct{}
-
-func (*createCallRNGGenerator) Execute(env Environment) {
-	// Create and call a random program
-	var (
-		code      = env.f.ByteSlice256()
-		isCreate2 = env.f.Bool()
-		callOp    = ops.OpCode(env.f.Byte())
-	)
-	env.p.CreateAndCall(code, isCreate2, callOp)
-}
-
-func (*createCallRNGGenerator) Importance() int {
-	return 4
-}
-
-type createCallGenerator struct{}
-
-func (*createCallGenerator) Execute(env Environment) {
-	// Prevent to deep recursion
-	if recursionLevel > maxRecursionLevel {
-		return
-	}
-	recursionLevel++
-	// Create and call a meaningful program
-	var (
-		seedLen   = env.f.Uint16()
-		seed      = env.f.ByteSlice(int(seedLen))
-		newFiller = filler.NewFiller(seed)
-		_, code   = GenerateProgram(newFiller)
-		isCreate2 = env.f.Bool()
-		callOp    = ops.OpCode(env.f.Byte())
-	)
-	env.p.CreateAndCall(code, isCreate2, callOp)
-	// Decreasing recursion level generates to heavy test cases,
-	// so once we reach maxRecursionLevel we don't create new CreateAndCalls.
-
-	// recursionLevel--
-}
-
-func (*createCallGenerator) Importance() int {
-	return 5
-}
-
-type randomCallGenerator struct{}
-
-func (*randomCallGenerator) Execute(env Environment) {
-	// Call a random address
-	var addr common.Address
-	if env.f.Bool() {
-		// call a precompile
-		addr = common.BigToAddress(new(big.Int).Mod(env.f.BigInt16(), big.NewInt(20)))
-	} else {
-		addr = common.BytesToAddress(env.f.ByteSlice(20))
-	}
-
-	c := precompiles.CallObj{
-		Gas:       env.f.GasInt(),
-		Address:   addr,
-		Value:     env.f.BigInt16(),
-		InOffset:  uint32(env.f.Uint16()),
-		InSize:    uint32(env.f.Uint16()),
-		OutOffset: uint32(env.f.Uint16()),
-		OutSize:   uint32(env.f.Uint16()),
-	}
-	precompiles.CallRandomizer(env.p, env.f, c)
-}
-
-func (*randomCallGenerator) Importance() int {
-	return 4
-}
-
-type callPrecompileGenerator struct{}
-
-func (*callPrecompileGenerator) Execute(env Environment) {
-	precompiles.CallPrecompile(env.p, env.f)
-}
-
-func (*callPrecompileGenerator) Importance() int {
-	return 8
 }
 
 type pushGenerator struct{}
