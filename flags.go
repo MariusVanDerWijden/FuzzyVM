c54b2c5e7d41a06e3054dfd8e0f24e3d5f2d1489
Marius van der Wijden <m.vanderwijden@live.de>
2022-12-02 12:43:38 +0100

    all: refactor, remove execution focus on generation

--- flags.go
+++ flags.go
@@ -16,61 +16,17 @@
 
 package main
 
-import (
-	"runtime"
-
-	"gopkg.in/urfave/cli.v1"
-)
+import "github.com/urfave/cli/v2"
 
 var (
-	genThreadsFlag = cli.IntFlag{
-		Name:  "gen.threads",
+	countFlag = &cli.IntFlag{
+		Name:  "count",
+		Usage: "Number of tests that should be benched/executed/generated",
+	}
+
+	threadsFlag = &cli.IntFlag{
+		Name:  "threads",
 		Usage: "Number of generator threads started (default = 1)",
 		Value: 1,
-	}
-	execThreadsFlag = cli.IntFlag{
-		Name:  "exec.threads",
-		Usage: "Number of execution threads started (default = NumCPU()",
-		Value: runtime.NumCPU(),
-	}
-	maxTestsFlag = cli.IntFlag{
-		Name:  "gen.maxtests",
-		Usage: "Number of max tests generated",
-		Value: 5000,
-	}
-	minTestsFlag = cli.IntFlag{
-		Name:  "gen.mintests",
-		Usage: "Number of max tests that could fail",
-		Value: 1000,
-	}
-	buildFlag = cli.BoolFlag{
-		Name:  "build",
-		Usage: "If build is set we run go-fuzz-build",
-	}
-	execNoGen = cli.BoolFlag{
-		Name:  "exec",
-		Usage: "If exec is set, we only execute not generate new tests",
-	}
-	retestFlag = cli.StringFlag{
-		Name:  "retest",
-		Usage: "Rerun the specified test",
-		Value: "",
-	}
-	benchFlag = cli.IntFlag{
-		Name:  "bench",
-		Usage: "Number of tests that should be benched",
-	}
-	corpusFlag = cli.IntFlag{
-		Name:  "corpus",
-		Usage: "Number of corpus elements that should be created",
-	}
-	corpusMinimizeFlag = cli.BoolFlag{
-		Name:  "minimizeCorpus",
-		Usage: "If minimizeCorpus is set we run the corpus minimizer",
-	}
-	configFileFlag = cli.StringFlag{
-		Name:  "config",
-		Usage: "Path to the config file required to run FuzzyVM",
-		Value: "config.toml",
 	}
 )
--- go.mod
+++ go.mod
@@ -3,23 +3,14 @@ module github.com/MariusVanDerWijden/FuzzyVM
 go 1.15
 
 require (
-	github.com/VictoriaMetrics/fastcache v1.10.0 // indirect
-	github.com/btcsuite/btcd/btcec/v2 v2.2.1 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
-	github.com/edsrzf/mmap-go v1.1.0 // indirect
-	github.com/ethereum/go-ethereum v1.10.23
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/holiman/goevmlab v0.0.0-20220511132401-0a7fe67b5caf
-	github.com/korovkin/limiter v0.0.0-20220422174850-01f593e64cf7
-	github.com/naoina/toml v0.1.2-0.20170918210437-9fafd6967416
-	github.com/pkg/errors v0.9.1
-	github.com/prometheus/tsdb v0.10.0 // indirect
-	github.com/rivo/uniseg v0.3.4 // indirect
+	github.com/VictoriaMetrics/fastcache v1.12.0 // indirect
+	github.com/dvyukov/go-fuzz v0.0.0-20220726122315-1d375ef9f9f6 // indirect
+	github.com/ethereum/go-ethereum v1.10.26
+	github.com/holiman/goevmlab v0.0.0-20221201133036-b31966a5267d
+	github.com/korovkin/limiter v0.0.0-20221015170604-22eb1ceceddc
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
-	github.com/tklauser/go-sysconf v0.3.10 // indirect
-	github.com/tklauser/numcpus v0.5.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.11 // indirect
+	github.com/urfave/cli/v2 v2.23.5
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
-	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
-	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
-	gopkg.in/urfave/cli.v1 v1.20.0
+	golang.org/x/crypto v0.3.0
 )
